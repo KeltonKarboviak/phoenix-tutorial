a58d6348172da0c50d3dae14847b041de28b4a5e
Kelton Karboviak <kelton.karboviak@daveramsey.com>
2019-07-27 11:27:15 -0500

    Work through Channels guide

--- lib/tutorial_app_web/channels/user_socket.ex
+++ lib/tutorial_app_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule TutorialAppWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", TutorialAppWeb.RoomChannel
+  channel "room:*", TutorialAppWeb.RoomChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
@@ -15,8 +15,18 @@ defmodule TutorialAppWeb.UserSocket do
   #
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
-  def connect(_params, socket, _connect_info) do
+  def connect(%{"token" => "undefined"}, socket, _connect_info) do
     {:ok, socket}
+  end
+
+  def connect(%{"token" => token}, socket, _connect_info) do
+    # max_age: 1209600 is equivalent to two weeks in seconds
+    case Phoenix.Token.verify(socket, "user socket", token, max_age: 1209600) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :current_user, user_id)}
+      {:error, _reason} ->
+        :error
+    end
   end
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
